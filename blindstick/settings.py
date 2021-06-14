8c564c13188fe4597f533169ee5f5a9eeb03b031
Todds Group <github@toddsgroup.com>
2021-06-13 17:17:14 -0700

    static file fixed

--- blindstick/settings.py
+++ blindstick/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = (
 )
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static_file")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
